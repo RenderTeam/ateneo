8270e4720fe89a551c69d0879b26f5a27db924c9
luricardorod <luricardorod@gmail.com>
2014-09-28 06:21:51 -0500

    partial vie add events

--- sections/study_groups/administrate-study-group.controller.browser.js
+++ sections/study_groups/administrate-study-group.controller.browser.js
@@ -2,7 +2,8 @@
 'use strict';
 
 module.exports = function (app) {
-  function administrateStudyGroup(scope, routeParams, StudyGroups) {
+  function administrateStudyGroup(scope, routeParams, StudyGroups, Users) {
+
     var searchParams = {
       condition: {
         _id: routeParams.groupId
@@ -14,8 +15,37 @@ module.exports = function (app) {
         scope.studyGroup = response.data[0];
         console.log(response);
       });
+
+    Users.search()
+      .success(function (response) {
+        scope.users = response.data;
+        console.log( response );
+      });
+
+    scope.saveNewUser = function  () {
+      console.log( scope.newUser );
+      scope.studyGroup.users.push( scope.newUser.username );
+
+      var updateParams = {
+        condition: {
+          _id: routeParams.groupId
+        },
+        reference : scope.studyGroup
+      };
+
+      StudyGroups.update( updateParams )
+        .success(function (response) {
+          console.log(response);
+          scope.hideNewUser = false;
+        });
+    };
+
+    scope.saveNewEvent = function  () {
+      console.log( scope.newEvent.date + ':' +scope.hour + ':' + scope.minute);
+    };
+
   }
 
   app.controller('AdministrateStudyGroup', administrateStudyGroup);
-  administrateStudyGroup.$inject = ['$scope', '$routeParams', 'StudyGroups'];
+  administrateStudyGroup.$inject = ['$scope', '$routeParams', 'StudyGroups', 'Users'];
 };
