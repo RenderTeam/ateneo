bbd927db3b5b3317a4774e5959315135849125fe
ardroz <darr.0725@gmail.com>
2014-09-28 08:44:29 -0500

    :smile: Creation fo the study groups

--- sections/study_groups/new-study-group.controller.browser.js
+++ sections/study_groups/new-study-group.controller.browser.js
@@ -1,19 +1,58 @@
 /*jslint node: true, indent: 2,nomen:true */
 'use strict';
+var __ = require('underscore')._;
 
 module.exports = function (app) {
-  function newStudyGroup(scope, location) {
+  function newStudyGroup(scope, location, Users, Auth, StudyGroups) {
+    var searchParams = {
+      fields: ['username', 'mail']
+    };
+
+    scope.usersAdded = [];
+
+    Users.search(searchParams)
+      .success(function (response) {
+        scope.users = response.data;
+      });
+
+    scope.addUser = function () {
+      if (scope.selectedUser) {
+        var flag = __.some(scope.usersAdded, function (user) {
+          return user.username === scope.selectedUser.username;
+        });
+
+        if ( !flag ) {
+          scope.usersAdded.push(scope.selectedUser);
+        }
+      }
+    };
+
+    scope.removeUser = function (index) {
+      scope.usersAdded.splice(index, 1);
+    };
 
     scope.createStudyGroup = function () {
+      var users = __.map( scope.usersAdded, function (user) {
+        return user.username;
+      });
 
-    };
-    
-    scope.RegisterNewStudyGroup = function () {
-      location.path('/study-groups');
-    };
+      var studyGroupParams = {
+        reference: {
+          name: scope.study_group.name,
+          admin: Auth.user.username,
+          users: users
+        }
+      };
 
+      StudyGroups.new(studyGroupParams)
+        .success(function (response) {
+          if (response.status) {
+            location.path('/study-groups/' + response._id);
+          }
+        });
+    };
   }
 
   app.controller('NewStudyGroup', newStudyGroup);
-  newStudyGroup.$inject = ['$scope', '$location'];
+  newStudyGroup.$inject = ['$scope', '$location', 'Users', 'Auth', 'StudyGroups'];
 };
