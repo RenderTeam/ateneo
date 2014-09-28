70f94a511d0973de77e70d8f4e5e76088be7f42b
ardroz <darr.0725@gmail.com>
2014-09-28 05:17:47 -0500

    :smile: Stop the /app.min.js track

--- sections/login/login.controller.browser.js
+++ sections/login/login.controller.browser.js
@@ -11,7 +11,7 @@ module.exports = function (app) {
           scope.loading = false;
           scope.errorInLogin = false;
           if (user.access) {
-            location.path('/wizard');
+            location.path('/profile');
           } else {
             scope.user = {};
             scope.errorInLogin = true;
